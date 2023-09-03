f3fe4abe9081439a6fe6c6016388716ad3020bae
Jordan Kovacs <98484926+JorKov-JAC@users.noreply.github.com>
2023-09-03 14:20:59 -0400

    Increase particle position precision

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-const SQRT_PARTICLE_COUNT = 10
+const SQRT_PARTICLE_COUNT = 32
 const PARTICLE_COUNT = SQRT_PARTICLE_COUNT * SQRT_PARTICLE_COUNT
 const PARTICLE_SIZE = .025;
 
@@ -51,6 +51,10 @@ function getUniformLoc(gl: WebGLRenderingContext, uniformName: string) {
 	return program[uLocs][uniformName]
 }
 
+function randClampUint8() {
+	return Math.random() * 256 - .5
+}
+
 function createParticleTexture(gl: WebGLRenderingContext) {
 	const texture = gl.createTexture()
 	gl.bindTexture(gl.TEXTURE_2D, texture)
@@ -60,7 +64,7 @@ function createParticleTexture(gl: WebGLRenderingContext) {
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
 
-	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, SQRT_PARTICLE_COUNT, SQRT_PARTICLE_COUNT, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8ClampedArray(([...new Array(PARTICLE_COUNT).keys()]).flatMap(() => [Math.random() * 256 - .5, Math.random() * 256 - .5, Math.random() * 256 - .5, Math.random() * 256 - .5])))
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, SQRT_PARTICLE_COUNT * 2, SQRT_PARTICLE_COUNT, 0, gl.RGB, gl.UNSIGNED_BYTE, new Uint8ClampedArray(([...new Array(PARTICLE_COUNT).keys()]).flatMap(() => [randClampUint8(), randClampUint8(), randClampUint8(), randClampUint8(), randClampUint8(), randClampUint8()])))
 
 	return texture
 }
@@ -77,8 +81,14 @@ const float H = ${SQRT_PARTICLE_COUNT}.;
 
 void main() {
 	float pIdx = floor(a_idx * .25);
-	vec2 pIdx2D = vec2(mod(pIdx, W) + (1./W)*.5, pIdx / W + (1./H)*.5);
-	vec2 pPos = texture2DLod(u_particles, pIdx2D / vec2(W, H), 0.).xy * 2. - 1.;
+	vec2 pIdx2D = vec2(mod(pIdx, W) * 2. + .5, pIdx / W + .5);
+	
+	vec2 toNorm = 1. / vec2(W * 2., H);
+	vec4 sample0 = texture2DLod(u_particles, pIdx2D * toNorm, 0.);
+	vec4 sample1 = texture2DLod(u_particles, (pIdx2D + vec2(1., 0.)) * toNorm, 0.);
+
+	vec2 pPos = (sample0.xy + vec2(sample0.z, sample1.z)/256.) * 2. - 1.;
+	// pPos = sample0.xy * 2. - 1.;
 
 	v_off = vec2(mod(a_idx, 2.) < .5 ? -1. : 1., mod(a_idx, 4.) < 1.5 ? -1. : 1.);
 
@@ -107,7 +117,7 @@ void main() {
 uniform sampler2D u_particles;
 
 void main() {
-	gl_FragColor = vec4(texture2D(u_particles, gl_FragCoord.xy / ${SQRT_PARTICLE_COUNT}., 0.).xy, 0., 1.);
+	gl_FragColor = vec4(texture2D(u_particles, gl_FragCoord.xy / vec2(${SQRT_PARTICLE_COUNT * 2}., ${SQRT_PARTICLE_COUNT}.) / 4., 0.).xyz, 1.);
 }
 `)
 fGl.bindAttribLocation(fGl_internalsProg, 0, "a_pos")
@@ -123,16 +133,29 @@ precision highp float;
 
 uniform highp sampler2D u_particles;
 
+const float SQRT_PARTICLE_COUNT = ${SQRT_PARTICLE_COUNT}.;
+
 void main() {
-	vec4 pv = texture2D(u_particles, gl_FragCoord.xy / ${SQRT_PARTICLE_COUNT}., 0.);
-	vec2 acc = pv.zw * 2. - 1.;
-	pv.xy += acc / 64.;
+	float odd = floor(mod(gl_FragCoord.x, 2.));
+	vec2 coord = vec2(gl_FragCoord.x - odd, gl_FragCoord.y);
+
+	vec2 toNorm = 1. / vec2(SQRT_PARTICLE_COUNT * 2., SQRT_PARTICLE_COUNT);
+	vec4 sample0 = texture2D(u_particles, coord * toNorm, 0.);
+	vec4 sample1 = texture2D(u_particles, (coord + vec2(1., 0.)) * toNorm, 0.);
+
+	vec4 pv = vec4(sample0.xy + vec2(sample0.z, sample1.z) / 256., sample1.xy);
+	vec2 vel = pv.zw * 2. - 1.;
+	pv.xy += vel / 256.;
 	if (pv.x > 1.) pv.z = .5 - abs(pv.z - .5);
 	if (pv.y > 1.) pv.w = .5 - abs(pv.w - .5);
 	if (pv.x < 0.) pv.z = .5 + abs(pv.z - .5);
 	if (pv.y < 0.) pv.w = .5 + abs(pv.w - .5);
 	pv.xy = clamp(pv.xy, 0., 1.);
-	gl_FragColor = pv;
+	
+	vec2 highPos = floor(pv.xy * 255.) / 255.;
+	vec2 lowPos = (pv.xy - highPos) * 256.;
+	// vec2 lowPos = fract(pv.xy * 255.);
+	gl_FragColor = vec4(highPos.xy, lowPos.x, 1.) * (1. - odd) + vec4(pv.zw, lowPos.y, 1.) * odd;
 }
 `)
 fGl.bindAttribLocation(fGl_particleProg, 0, "a_pos")
@@ -185,7 +208,7 @@ for (const i of [0,1]) {
 fGl.clearColor(0,0,0,1)
 function fGlLoop() {
 	fGl.bindFramebuffer(fGl.FRAMEBUFFER, fGl_fbos[fGl_tick % fGl_fbos.length]!)
-	fGl.viewport(0, 0, SQRT_PARTICLE_COUNT, SQRT_PARTICLE_COUNT)
+	fGl.viewport(0, 0, SQRT_PARTICLE_COUNT * 2, SQRT_PARTICLE_COUNT)
 	fGl.useProgram(fGl_particleProg)
 	fGl.bindTexture(fGl.TEXTURE_2D, fGl_texs[(fGl_tick + 1) % fGl_texs.length]!)
 	fGl.bindBuffer(fGl.ARRAY_BUFFER, fParticleBuff)
@@ -193,17 +216,9 @@ function fGlLoop() {
 	fGl.vertexAttribPointer(getAttribLoc(fGl, "a_pos"), 2, fGl.FLOAT, false, 0, 0)
 	fGl.drawArrays(fGl.TRIANGLE_STRIP, 0, 4)
 
-	// fGl.bindBuffer(fGl.ARRAY_BUFFER, fParticleBuff)
-	// fGl.bindFramebuffer(fGl.FRAMEBUFFER, null)
-	// fGl.viewport(0, 0, fGl.canvas.width, fGl.canvas.height)
-	// fGl.bindTexture(fGl.TEXTURE_2D, fGl_texs[fGl_tick % fGl_texs.length]!)
-	// fGl.useProgram(fGl_internalsProg)
-	// fGl.drawArrays(fGl.TRIANGLE_STRIP, 0, 4)
-
 	fGl.bindFramebuffer(fGl.FRAMEBUFFER, null)
 	fGl.viewport(0, 0, fGl.canvas.width, fGl.canvas.height)
 	fGl.clear(fGl.COLOR_BUFFER_BIT)
-
 	fGl.enable(fGl.BLEND)
 	fGl.blendEquation(fGl.FUNC_ADD)
 	fGl.blendFunc(fGl.ONE, fGl.ONE)
@@ -214,6 +229,15 @@ function fGlLoop() {
 	fGl.vertexAttribPointer(getAttribLoc(fGl, "a_idx"), 1, fGl.FLOAT, false, 0, 0)
 	fGl.drawArrays(fGl.TRIANGLES, 0, PARTICLE_COUNT * 6)
 	fGl.disable(fGl.BLEND)
+
+	fGl.bindFramebuffer(fGl.FRAMEBUFFER, null)
+	fGl.viewport(0, 0, SQRT_PARTICLE_COUNT * 2 * 4, SQRT_PARTICLE_COUNT * 4)
+	fGl.useProgram(fGl_internalsProg)
+	fGl.bindBuffer(fGl.ARRAY_BUFFER, fParticleBuff)
+	fGl.enableVertexAttribArray(getAttribLoc(fGl, "a_pos"))
+	fGl.vertexAttribPointer(getAttribLoc(fGl, "a_pos"), 2, fGl.FLOAT, false, 0, 0)
+	fGl.bindTexture(fGl.TEXTURE_2D, fGl_texs[fGl_tick % fGl_texs.length]!)
+	fGl.drawArrays(fGl.TRIANGLE_STRIP, 0, 4)
 
 	++fGl_tick
 	requestAnimationFrame(fGlLoop)
